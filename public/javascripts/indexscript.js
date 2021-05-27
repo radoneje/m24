4f2c51d473592cda86c5cb6b75cda42ee2556229
radoneje <den.shevchenko@gmail.com>
2021-05-27 16:06:48 +0300

    d

--- public/javascripts/indexscript.js
+++ public/javascripts/indexscript.js
@@ -8,11 +8,184 @@ var mainHeadApp = new Vue({
         panels:{rundown:true, dashboard:false},
         tabs:{archive:false,video:false,messager:false,rss:false},
         tabIsOpen:false,
-        programs:[{id:1,title:'Новости 24', selected:true}],
-        news:{},
-        currNews:{},
+        programs:[{id:1,title:'Новости 24', isSelected:true}],
+        news:[],
+        selNews:{},
+        settings:{},
+        loader:false,
+        plots:[],
     },
     methods: {
+        selectPlotAutor:async function(item){
+            console.log("selectPlotAutor", item)
+            closeAllPopup();
+            var elem=createPopap(document.getElementById("plotAutors"+item.id))
+
+            elem.classList.add("selectPlotAutorsDlg")
+            this.users.forEach(u=>{
+                var box=document.createElement("div")
+                elem.appendChild(box)
+
+                box.classList.add("listItem")
+                box.classList.add("listItemFlex")
+
+                var author=document.createElement("div")
+                var camera=document.createElement("div")
+                var editor=document.createElement("div")
+                var name=document.createElement("div")
+
+                author.classList.add("listIcon")
+                author.classList.add("iconAutor")
+                camera.classList.add("listIcon")
+                camera.classList.add("iconCamera")
+                editor.classList.add("listIcon")
+                editor.classList.add("iconEditor")
+
+                name.innerText=this.getUserNameFromId(u.id);
+                box.setAttribute("userid", u.id);
+                
+
+                box.appendChild(author);
+                box.appendChild(camera);
+                box.appendChild(editor);
+                box.appendChild(name);
+
+
+            })
+
+
+        },
+        changePlot:async function(item){
+            console.log("changeplot", item)
+            var res=await axios.patch("/rest/api/v1/plot/",item);
+        },
+        clickNews:async function(item){
+            this.loader=true;
+            this.selNews=item;
+            this.plots=[];
+            var res=await axios.get("/rest/api/v1/plot/"+item.id);
+            console.log(res.data)
+            this.plots=res.data;
+            this.loader=false;
+        },
+        addPlot:async function() {
+            if (this.loader || !this.selNews.id)
+                return;
+            this.loader = true;
+            var res = await axios.post("/rest/api/v1/plot/", {newsid: this.selNews.id})
+            this.loader = false;
+            this.plots.push(res.data);
+            this.plots.sort((a,b)=>(a.sort-b.sort));
+        },
+        chekNewsFire:function(item){
+
+            return moment(item.date).unix()> moment().unix() &&  moment(item.date).unix()< moment().endOf("day").unix()
+        },
+        chekNewsPast:function(item){
+           return moment(item.date).unix()< moment().unix()
+        },
+        getUserNameFromId:function (id){
+            var user=this.getUserFromId(id);
+            return user?(user.suName+" "+ user.name):null;
+        },
+        getUserFromId:function (id){
+            var ret=this.users.filter(u=>u.id==id);
+            if(ret.length>0)
+                return ret[0];
+            else
+                return null;
+        },
+        changeNewsOwner:function(){
+            var elem=createPopap(document.getElementById("NewsOwner"));
+            elem.classList.add("popupDlg")
+            this.users.forEach(u=>{
+                var item=document.createElement('div')
+                item.classList.add("listItem")
+                item.innerText=u.suName +" " + u.name;
+                elem.appendChild(item)
+                item.onclick=async ()=>{
+                    this.selNews.ownerid=u.id;
+                    closeAllPopup();
+                    await this.newsChange(this.selNews)
+
+                }
+            })
+        },
+        changeNewsTime:async function(event){
+            if(!this.selNews.date)
+                return;
+            let val=event.currentTarget.value.replace(/[^\d]/g, "")
+            let m=val.match(/(\d{1,2})(\d\d)$/)
+            if(m){
+                this.selNews.date=
+                    moment(this.selNews.date)
+                        .startOf('day')
+                        .add(m[1],"hours")
+                        .add(m[2],"minutes")
+                        .toDate()
+                await this.newsChange(this.selNews)
+            }
+        },
+        changeNewsDate:async function(){
+
+            if(!this.selNews.date)
+                return;
+            closeAllPopup();
+
+            var elem=document.createElement('div');
+            elem.classList.add('popUpMenuWindow')
+            elem.classList.add('popup')
+            elem.id="prgDate"
+            elem.addEventListener("click",(e)=>{e.stopPropagation()})
+            var parent=document.getElementById("PrgDate");
+            parent.appendChild(elem);
+            var btn=document.createElement('div');
+            btn.id="btn"
+            elem.appendChild(btn)
+            let first=true;
+            new Datepicker('#btn', {
+                inline: true,
+                openOn:this.selNews.date,
+                weekStart:this.settings.weekStart,
+                onChange:async (e)=>{
+                    if(!first) {
+                        let old = moment(this.selNews.date)
+                        var date = moment(e).startOf('day');
+                        this.selNews.date = date.add(old.format("HH"), 'hours').add(old.format("mm"), 'minutes').toDate();
+                        await this.newsChange(this.selNews)
+                        first=true
+                        closeAllPopup();
+                    }
+                    else
+                        first=false;
+
+                }
+            });
+
+        },
+        newsChange:async function (selNews) {
+
+            var r=await axios.patch("/rest/api/v1/news/",selNews);
+            this.news.sort((a,b)=>{return moment(b.date).unix()-moment(a.date).unix()})
+        },
+        reloadProg:async function () {
+            let res=(await axios.get("/rest/api/v1/prog/"));
+
+            let selected=null;
+            let sel=this.programs.filter(p=>p.isSelected)
+            if(sel.length>0)
+                selected=sel[0].id;
+            res.data.forEach(p=>{
+                p.isSelected=p.id==selected;
+            });
+            if( res.data.filter(p=>p.isSelected).length==0)
+                res.data[0].isSelected=true;
+           this.programs=res.data;
+        },
+        getSelProg:function(){
+            let sel=this.programs.filter(p=>p.isSelected)
+            return sel[0];
+        },
         exitClick:async function () {
             await axios.delete("/rest/api/v1/user")
             document.location.href="/login";
@@ -47,8 +220,7 @@ var mainHeadApp = new Vue({
             });
         },
        reloadNews:async function(){
-           var _this=this;
-            var r=await axios.get("/rest/api/v1/news/"+  _this.programs.filter(p=>p.selected)[0].id);
+            var r=await axios.get("/rest/api/v1/news/"+  this.getSelProg().id);
             this.news=r.data;
        },
         newsAdd:async  function(){
@@ -57,18 +229,6 @@ var mainHeadApp = new Vue({
             _this.currNews=r.data;
            // console.log(r.data);
         },
-        newsClick:async function(newsItem){
-
-            this.news.forEach(n=>{
-                if(n.id!=newsItem.id)
-                    n.active=false;
-                else {
-                    n.active = true;
-                    this.currNews=n;
-                }
-                return n;
-            });
-        }
 
     },
     computed:{
@@ -79,10 +239,18 @@ var mainHeadApp = new Vue({
     },
     mounted: async function () {
         var _this=this;
-        var r=await axios.get("/rest/api/v1/users")
+        let r=await axios.get("/rest/api/v1/users")
+        r.data.sort(function(a, b){
+            if(a.suName < b.suName) { return -1; }
+            if(a.suName > b.suName) { return 1; }
+            return 0;
+        })
         _this.users=r.data;
+         r=await axios.get("/rest/api/v1/settings")
+         this.settings=r.data;
 
         _this.user=user;
+        await this.reloadProg();
         _this.reloadNews();
 
         var socket = io('http://localhost');
@@ -105,7 +273,7 @@ var mainHeadApp = new Vue({
                // }
         });
 
-
+    window.addEventListener("click",()=>{closeAllPopup()})
     }
 });
 function validateEmail(email) {
@@ -159,6 +327,19 @@ function initEvents(_this){
            // console.log("newsChange socket", data.id,  )
         }
     }
+}
+function createPopap(parent) {
+    var elem=document.createElement('div');
+    elem.classList.add('popup')
+    elem.addEventListener("click",(e)=>{e.stopPropagation()})
+    parent.appendChild(elem);
+    return elem;
+}
+function closeAllPopup() {
+    var elems=document.querySelectorAll(".popup");
+    elems.forEach(e=>{
+        e.parentNode.removeChild(e);
+    })
 }
 
 
--- routes/api.js
+++ routes/api.js
@@ -7,7 +7,7 @@ router.all('/ping', async (req, res, next)=>{
   res.json(req.clients.length)
 });
 router.post('/login', async (req, res, next)=> {
-  return res.status(404).send("not found")
+ // return res.status(404).send("not found")
   delete req.session.user;
   var q=await req.knex
       .select("*")
@@ -55,6 +55,37 @@ router.delete("/user", async (req, res)=> {
   return res.json({status:1});
 
 })
+
+router.post("/plot", async (req, res)=> {
+  if (!req.session || !req.session.user)
+    return res.status(401.7).json({status: -1, msg: "access deny"});
+  var ret=await req.knex("t_plot").insert({newsid:req.body.newsid},"*");
+
+  var dt=await req.knex.select("*").from("t_plot").where({newsid:req.body.newsid}).orderBy("sort");
+  let i=0;
+  for(var item of dt){
+    i=i+10;
+    await req.knex("t_plot").update({sort:i}).where({id:item.id})
+  }
+  return res.json(ret[0]);
+})
+router.get("/plot/:newsid", async (req, res)=> {
+  if (!req.session || !req.session.user)
+    return res.status(401.7).json({status: -1, msg: "access deny"});
+
+  var ret=await req.knex.select("*").from("v_plot").where({newsid:req.params.newsid}).orderBy("sort")
+
+
+  return res.json(ret);
+})
+router.patch("/plot", async (req, res)=> {
+  if (!req.session || !req.session.user)
+    return res.status(401.7).json({status: -1, msg: "access deny"});
+
+  var ret=await req.knex("t_plot").update({title:req.body.title},"*").where({id:req.body.id});
+
+  return res.json(ret[0]);
+})
 router.post("/user", async (req, res)=> {
   if (!req.session || !req.session.user)
     return res.status(401.7).json({status: -1, msg: "access deny"});
@@ -85,6 +116,23 @@ delete usr.id;
     return res.json({e:e.message});
   }
 })
+
+
+router.get("/settings", async (req, res)=> {
+  var setting=require("../settings")
+  res.json(setting);
+});
+router.get("/prog", async (req, res)=> {
+  if (!req.session || !req.session.user)
+    return res.status(401.7).json({status: -1, msg: "access deny"});
+  var q=await req.knex
+      .select("*")
+      .from("t_prog")
+      .where({isDeleted:false})
+      .orderBy('date' , 'desc');
+
+  return  res.json(q);
+});
 router.get("/news/:progId", async (req, res)=> {
   if (!req.session || !req.session.user)
     return res.status(401.7).json({status: -1, msg: "access deny"});
@@ -103,6 +151,19 @@ router.post("/news", async (req, res)=> {
       .insert(req.body, "*");
 
   req.clients.send("newsChange", q[0])
+  return  res.json(q[0]);
+});
+
+router.patch("/news", async (req, res)=> {
+  if (!req.session || !req.session.user)
+    return res.status(401.7).json({status: -1, msg: "access deny"});
+
+  let id=req.body.id;
+  delete req.body.id;
+  var q=await req.knex("t_news")
+      .update(req.body, "*")
+      .where({id:id})
+  //req.clients.send("newsChange", q[0])
   return  res.json(q[0]);
 });
 
